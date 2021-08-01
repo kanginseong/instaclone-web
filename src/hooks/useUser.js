483a5960936a0eb791236ed38ebf689404b5414c
insung <inh3011@gmail.com>
2021-08-01 13:27:14 +0900

    Photo Component

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -17,16 +17,13 @@ function useUser() {
   const { data } = useQuery(ME_QUERY, {
     skip: !hasToken,
   });
-
   // 'useEffect' runs once a hook is mounted, every time the data changes.
   useEffect(() => {
     if (data?.me === null) {
       // there is a token on LocalStorge but the token did not work on the backend.
-      // logUserOut();
-      console.log(data);
+      logUserOut();
     }
   }, [data]);
-
   return { data };
 }
 
